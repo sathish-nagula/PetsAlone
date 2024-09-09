a9e6f9b58c29344149ec1aba8afbbeda431c44fa
Sathish <sathish.nagula@outlook.com>
2024-09-09 05:57:06 +0100

    Account controller

---
+++ PetsAlone/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using PetsAlone.Utils;
+using System.Security.Claims;
+
+namespace PetsAlone.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == Secrets.userName && password == Secrets.password)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, username)
+        };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        ModelState.AddModelError("", "Invalid username or password");
+        return View();
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ PetsAlone/Utils/Secrets.cs
@@ -0,0 +1,8 @@
+﻿namespace PetsAlone.Utils
+{
+    public static class Secrets
+    {
+        public const string userName = "elmyraduff";
+        public const string password = "MontanaMax!!";
+    }
+}
