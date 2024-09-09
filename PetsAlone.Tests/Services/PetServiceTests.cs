c6b6f396b767ba1da361f5b87f0e97db43a46f20
Sathish <sathish.nagula@outlook.com>
2024-09-09 05:27:59 +0100

    Implemented total pets count and unit test

---
+++ PetsAlone.Tests/Services/PetServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using PetsAlone.Models;
+using PetsAlone.Models.Enums;
+using PetsAlone.ServiceContracts;
+using PetsAlone.Services;
+using System.Data;
+
+namespace PetsAlone.Tests.Services;
+
+[TestFixture]
+public class PetServiceTests
+{
+    private Mock<IDataLoader> _dataLoaderMock;
+    private PetService _petService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dataLoaderMock = new Mock<IDataLoader>();
+        _petService = new PetService(_dataLoaderMock.Object);
+    }
+
+    [Test]
+    public async Task GetTotalPetsCountAsync_ReturnsCorrectCount()
+    {
+        var pets = new List<Pet>
+        {
+            new Pet { Id = 1, Name = "Cat 1", PetType = PetType.Cat },
+            new Pet { Id = 2, Name = "Dog 1", PetType = PetType.Dog }
+        };
+        _dataLoaderMock.Setup(dl => dl.LoadPetsAsync()).ReturnsAsync(pets);
+        _petService = new PetService(_dataLoaderMock.Object);
+
+        var count = await _petService.GetTotalPetsCountAsync("Dog");
+
+        Assert.That(count, Is.EqualTo(1));
+    }
+
+}
---
+++ PetsAlone/Services/PetService.cs
@@ -0,0 +1,47 @@
+﻿using PetsAlone.Models;
+using PetsAlone.Models.Enums;
+using PetsAlone.ServiceContracts;
+
+namespace PetsAlone.Services;
+
+public class PetService : IPetService
+{
+    private readonly IDataLoader _dataLoader;
+    private static List<Pet> _pets;
+
+    public PetService(IDataLoader dataLoader)
+    {
+        _dataLoader = dataLoader;
+        LoadPetsAsync();
+    }
+
+    private async Task LoadPetsAsync()
+    {
+        _pets = await _dataLoader.LoadPetsAsync();
+    }
+
+
+    public async Task<int> GetTotalPetsCountAsync(string? typeFilter)
+    {
+        if (_pets == null || !_pets.Any()) await LoadPetsAsync();
+
+        var petsQuery = _pets.AsQueryable();
+
+        if (!string.IsNullOrEmpty(typeFilter) && Enum.TryParse(typeFilter, out PetType petType))
+        {
+            petsQuery = petsQuery.Where(p => p.PetType == petType);
+        }
+
+        return await Task.FromResult(petsQuery.Count());
+    }
+
+    public Task<List<Pet>> GetPetsAsync(string typeFilter, string sortOrder, int page = 1, int pageSize = 10)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddPetAsync(Pet pet)
+    {
+        throw new NotImplementedException();
+    }
+}
