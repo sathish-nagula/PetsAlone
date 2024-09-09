4be4fb62ed787af16afabdc7c6dbb1c6f213b531
Sathish <sathish.nagula@outlook.com>
2024-09-09 05:16:37 +0100

    Implemented the IDataLoader interface in this  service class DataLoader, so it does lend itself well to unit testing or to swap with a database-backed implementation. Generated the data in code for now.

---
+++ PetsAlone/Services/DataLoader.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using PetsAlone.Models;
+using PetsAlone.Models.Enums;
+using PetsAlone.ServiceContracts;
+
+namespace PetsAlone.Services;
+
+public class DataLoader : IDataLoader
+{
+    private static readonly List<Pet> _pets;
+
+    // TO DO: Repalce the fake data loader with DbContext
+    static DataLoader()
+    {
+        _pets = GeneratePets();
+    }
+
+    public Task<List<Pet>> LoadPetsAsync()
+    {
+        return Task.FromResult(_pets);
+    }
+
+    private static List<Pet> GeneratePets()
+    {
+        var faker = new Faker<Pet>()
+            .RuleFor(p => p.Id, f => f.IndexFaker + 1)
+            .RuleFor(p => p.Name, f => f.Name.FirstName())
+            .RuleFor(p => p.PetType, f => f.PickRandom<PetType>())
+            .RuleFor(p => p.Description, f => f.Lorem.Sentence())
+            .RuleFor(p => p.IsMissing, f => true)
+            .RuleFor(p => p.DateMissing, f => f.Date.Past(2));
+
+        return faker.Generate(100);
+    }
+}
