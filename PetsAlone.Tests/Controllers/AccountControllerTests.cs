5658ee90add13ae7577c913d6161133d305e62e2
Sathish <sathish.nagula@outlook.com>
2024-09-09 06:23:36 +0100

    Unit tests

---
+++ PetsAlone.Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PetsAlone.Controllers;
+
+namespace PetsAlone.Tests.Controllers;
+
+[TestFixture]
+public class AccountControllerTests
+{
+    private Mock<IAuthenticationService> _authServiceMock;
+    private AccountController _accountController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _authServiceMock = new Mock<IAuthenticationService>();
+        _accountController = new AccountController();
+    }
+
+    [Test]
+    public void Login_ReturnsView()
+    {
+        var result = _accountController.Login();
+
+        Assert.That(result, Is.TypeOf<ViewResult>());
+    }
+}
---
+++ PetsAlone.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PetsAlone.Controllers;
+using PetsAlone.Models;
+using PetsAlone.ServiceContracts;
+
+namespace PetsAlone.Tests.Controllers;
+
+[TestFixture]
+public class HomeControllerTests
+{
+    private Mock<IPetService> _petServiceMock;
+    private HomeController _homeController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _petServiceMock = new Mock<IPetService>();
+        _homeController = new HomeController(_petServiceMock.Object);
+    }
+
+    [Test]
+    public async Task Index_ReturnsViewWithPets()
+    {
+        _petServiceMock.Setup(ps => ps.GetPetsAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()))
+            .ReturnsAsync(new List<Pet> { new Pet { Id = 1, Name = "Missing Pet" } });
+        _petServiceMock.Setup(ps => ps.GetTotalPetsCountAsync(It.IsAny<string>()))
+            .ReturnsAsync(1);
+
+        var result = await _homeController.Index(null, null, 1);
+
+        var viewResult = result as ViewResult;
+        Assert.That(viewResult, Is.Not.Null);
+        Assert.That(viewResult.Model, Is.TypeOf<List<Pet>>());
+        Assert.That(((List<Pet>)viewResult.Model).Count, Is.EqualTo(1));
+    }
+}
