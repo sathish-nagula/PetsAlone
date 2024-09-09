d73d283605d86a37f4042daa2bf6f78c9f60ccf5
Sathish <sathish.nagula@outlook.com>
2024-09-09 06:22:37 +0100

    create

---
+++ PetsAlone/Views/Pet/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetsAlone.Views.Pet
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
