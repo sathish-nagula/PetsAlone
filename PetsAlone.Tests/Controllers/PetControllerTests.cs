028aee69f6fa7bd39964311e3524d99b88d8586b
Sathish <sathish.nagula@outlook.com>
2024-09-09 06:21:34 +0100

    Pet Controller

---
+++ PetsAlone.Tests/Controllers/PetControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PetsAlone.Controllers;
+using PetsAlone.Models;
+using PetsAlone.ServiceContracts;
+
+namespace PetsAlone.Tests.Controllers;
+
+[TestFixture]
+public class PetControllerTests
+{
+    private Mock<IPetService> _petServiceMock;
+    private PetController _petController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _petServiceMock = new Mock<IPetService>();
+        _petController = new PetController(_petServiceMock.Object);
+    }
+
+    [Test]
+    public void Create_Get_ReturnsView()
+    {
+        var result = _petController.Create();
+
+        Assert.That(result, Is.TypeOf<ViewResult>());
+    }
+
+    [Test]
+    public async Task Create_Post_ValidModel_RedirectsToHome()
+    {
+        var pet = new Pet { Name = "My Pet" };
+
+        var result = await _petController.Create(pet);
+
+        _petServiceMock.Verify(ps => ps.AddPetAsync(pet), Times.Once);
+        Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+        var redirectResult = (RedirectToActionResult)result;
+        Assert.That(redirectResult.ActionName, Is.EqualTo("Index"));
+        Assert.That(redirectResult.ControllerName, Is.EqualTo("Home"));
+    }
+
+    [Test]
+    public async Task Create_Post_InvalidModel_ReturnsViewWithModel()
+    {
+        var pet = new Pet { Name = "" };
+        _petController.ModelState.AddModelError("Name", "Required");
+
+        var result = await _petController.Create(pet);
+
+        Assert.That(result, Is.TypeOf<ViewResult>());
+        var viewResult = (ViewResult)result;
+        Assert.That(viewResult.Model, Is.EqualTo(pet));
+    }
+}
---
+++ PetsAlone/Controllers/PetController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PetsAlone.Models;
+using PetsAlone.ServiceContracts;
+
+namespace PetsAlone.Controllers;
+
+[Authorize]
+public class PetController : Controller
+{
+    private readonly IPetService _petService;
+
+    public PetController(IPetService petService)
+    {
+        _petService = petService;
+    }
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Pet pet)
+    {
+        if (ModelState.IsValid)
+        {
+            await _petService.AddPetAsync(pet);
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(pet);
+    }
+}
